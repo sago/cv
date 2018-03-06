49e38df9fed1b9e2d26d2fd0b0ce61c9768e704f
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-03-06 13:53:04 -0600

    feature/index-studies-skills pages added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def index
+  end
+
+  def studies
+  end
+
+  def skills
+  end
+
+  def porfolio
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :menus
+  get 'studies',    to: 'pages#studies',      as: 'studies'
+  get 'skills',     to: 'pages#skills',       as: 'skills'
+  get 'portfolio',  to: 'pages#portfolio',    as: 'portfolio'
+  get 'blog',       to: 'pages#blog',         as: 'blog'
+  get 'contact',    to: 'pages#contact',      as: 'contact'
+
+  # Routes for mailer
+  #match 'contact' => 'contact#new', :as => 'contact', :via => :get
+  #match 'contact' => 'contact#create', :as => 'contact', :via => :post
+
+
+  root to: 'pages#index'
+
 end
