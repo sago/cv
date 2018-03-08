8c8b0c442fd6530c37ad384256e53d58e7c80a2b
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-03-07 18:41:32 -0600

    feature/contact update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
+  get 'contacts/create'
+
   resources :menus
   get 'studies',    to: 'pages#studies',      as: 'studies'
   get 'skills',     to: 'pages#skills',       as: 'skills'
   get 'portfolio',  to: 'pages#portfolio',    as: 'portfolio'
   get 'blog',       to: 'pages#blog',         as: 'blog'
-  get 'contact',    to: 'pages#contact',      as: 'contact'
 
-  # Routes for mailer
-  #match 'contact' => 'contact#new', :as => 'contact', :via => :get
-  #match 'contact' => 'contact#create', :as => 'contact', :via => :post
+  # Routes for contact
+  resources 'contacts', only: [:new, :create], path_names: { new: '' }
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
 
   root to: 'pages#index'
