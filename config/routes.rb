e03f86833c5ae9aa974af12f70ff3fa9c5465a20
Saul Gomez <ing.saul.gomez18@gmail.com>
2018-03-07 19:12:25 -0600

    feature/contact update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'skills',     to: 'pages#skills',       as: 'skills'
   get 'portfolio',  to: 'pages#portfolio',    as: 'portfolio'
   get 'blog',       to: 'pages#blog',         as: 'blog'
+  get 'contact',    to: 'pages#contact',      as: 'contact'
 
   # Routes for contact
   resources 'contacts', only: [:new, :create], path_names: { new: '' }
